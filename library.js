cb5b8c3a1f487b064c1177f087ee34692518248e
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-08-26 19:14:00 +0500

    add all details of each object for complete output on each card

--- library.js
+++ library.js
@@ -20,17 +20,43 @@ function addBookToLibrary(book){
 }
 
 function displayAllBooks(myLibrary) {
+
     const shelf = document.querySelector('.shelf');
+   
     myLibrary.forEach(book => {
+
+        //main card for displaying details
         const card = document.createElement('div');
         card.classList.add('card');
         shelf.appendChild(card);
+        //card title 
         const title = document.createElement('h3');
         title.classList.add('title');
         card.appendChild(title);
         title.textContent = 'Title : ' + book.title;
+        //sepearte details wrapper for card details excluding title
+        const details = document.createElement('div');
+        details.classList.add('details');
+        card.appendChild(details);
 
-    
+        //rest of details starts here
+        //author
+        const author = document.createElement('p');
+        author.classList.add('author');
+        details.appendChild(author);
+        author.textContent = 'Author : ' + book.author;
+        
+        const pages = document.createElement('p');
+        pages.classList.add('pages');
+        details.appendChild(pages);
+        pages.textContent = 'Pages : ' + book.pages;
+        
+        const read = document.createElement('p');
+        read.classList.add('status');
+        details.appendChild(read);
+        read.textContent = 'Read Status: ' + book.read;
+
+
     });
 }
 
