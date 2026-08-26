fae2f91298d419351176c675739496af7fdaaede
ghaniafridi48 <pshfscmm2000548@gmail.com>
2026-08-26 18:40:49 +0500

    add card details to display dynamically

--- library.js
+++ library.js
@@ -19,7 +19,23 @@ function addBookToLibrary(book){
 
 }
 
+function displayAllBooks(myLibrary) {
+    const shelf = document.querySelector('.shelf');
+    myLibrary.forEach(book => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        shelf.appendChild(card);
+        const title = document.createElement('h3');
+        title.classList.add('title');
+        card.appendChild(title);
+        title.textContent = 'Title : ' + book.title;
+
+    
+    });
+}
+
 let b = new Book("book","me",10,"no");
 console.log(b);
 addBookToLibrary(b);
-console.log(myLibrary);
+
+displayAllBooks(myLibrary);
